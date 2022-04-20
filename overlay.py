01c434127f35a3d9493ed04910ec21a5daf2541a
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-04-19 21:07:56 -0400

    Fix fg end bug

--- overlay.py
+++ overlay.py
@@ -16,9 +16,9 @@ def overlay_grayscale(bg, fg, pos, b=0, c=255):
   bg_cropped = (bg[bg_y_start:bg_y_end, bg_x_start:bg_x_end] - b) / c
 
   fg_y_start = max(-pos[0], 0)
-  fg_y_end = min(bg.shape[0] - pos[0], fg.shape[0])
+  fg_y_end = min(max(0, bg.shape[0] - pos[0]), fg.shape[0])
   fg_x_start = max(-pos[1], 0)
-  fg_x_end = min(bg.shape[1] - pos[1], fg.shape[1])
+  fg_x_end = min(max(0, bg.shape[1] - pos[1]), fg.shape[1])
   fg_cropped = (fg[fg_y_start:fg_y_end, fg_x_start:fg_x_end] - b) / c
 
   result = bg.copy()
