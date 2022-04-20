27ebd28a1bcf23258b50b3c6039b2ac2b781d151
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-04-20 16:27:11 -0400

    Add xcom scraper

---
+++ xcom.py
@@ -0,0 +1,79 @@
+import json
+import matplotlib.pyplot as plt
+import requests
+from tqdm import tqdm
+from urllib import response
+
+
+def parse_attenuation_table(attenuations_text):
+  attenuations_list = [float(num) for num in attenuations_text.split()[5:]]
+  attenuations_keys = attenuations_list[::2]
+  attenuations_values = attenuations_list[1::2]
+  return dict(zip(attenuations_keys, attenuations_values))
+
+
+def get_total_attenuation(energies_kev, atomic_number=None,
+                          element_symbol=None):
+  if atomic_number is not None and element_symbol is not None:
+    raise TypeError('atomic_number and element_symbol should not be passed '
+                    'together')
+  if atomic_number is None and element_symbol is None:
+    raise TypeError('atomic_number or element_symbol should be passed')
+
+  energies_mev = [str(energy / 1000) for energy in energies_kev]
+  energy_min = min(energies_mev)
+  energy_max = max(energies_mev)
+
+  url = 'https://physics.nist.gov/cgi-bin/Xcom/data.pl'
+  data = {
+    'character': 'space',
+    'Method': '1',
+    'ZNum': atomic_number,
+    'ZSym': element_symbol,
+    'OutOpt': 'PIC',
+    'NumAdd': '1',
+    'Energies': ';'.join(map(str, energies_mev)),
+    'Output': '',
+    'WindowXmin': energy_min,
+    'WindowXmax': energy_max,
+    'with': 'on',
+  }
+
+  response_text = requests.post(url, data=data).text
+
+  if 'Error' in response_text:
+    error_text = response_text[108:-5]
+    raise RuntimeError(error_text)
+
+  return parse_attenuation_table(response_text)
+
+
+def get_attenuation_ratio(low_energy, high_energy, atomic_number=None,
+                          element_symbol=None):
+  attenuations = get_total_attenuation([low_energy, high_energy],
+                                       atomic_number,
+                                       element_symbol)
+  attenuations_list = list(attenuations.values())
+  return attenuations_list[0] / attenuations_list[1]
+
+
+def get_transition_ratios(low_energy, high_energy, inorganic_start=11,
+                          metal_start=19):
+  # inorganic_start and metal_start declare the effective atomic number of
+  # the first element that corresponds to that category. The threshold will be
+  # (ratio(category_start) + ratio(categry_start - 1)) / 2.
+
+  atomic_numbers = {'organic end': inorganic_start - 1,
+                    'inorganic start': inorganic_start,
+                    'inorganic end': metal_start - 1,
+                    'metal start': metal_start}
+
+  ratios = {key: get_attenuation_ratio(low_energy, high_energy,
+            atomic_number=value) for key, value in atomic_numbers.items()}
+
+  organic_inorganic_threshold = (ratios['organic end']
+                                   + ratios['inorganic start']) / 2
+  inorganic_metal_threshold = (ratios['inorganic end']
+                                 + ratios['metal start']) / 2
+
+  return organic_inorganic_threshold, inorganic_metal_threshold
