689d50d0128cbc1b2bf144644fa4d09249ede3a4
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-04-25 12:35:19 -0400

    Remove TODO that shouldn't have been there

--- colorizer.py
+++ colorizer.py
@@ -1,7 +1,6 @@
 # TODO: replace ratios better names, such as organic_inorganic_threshold
 # TODO: add fallback in case there's no internet connection
 # TODO: check exceptions when the xcom scraper fails to fetch info
-# TODO: add ratios cache
 # TODO: Optimize colorizer. Stop doing it pixelwise
 
 import cv2
