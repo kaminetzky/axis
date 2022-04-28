b626932a2509533e0ffc11acd251d0c8bc07fef2
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-04-28 18:47:13 -0400

    Add save_data to file.py

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 import cv2
 from functools import cache
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import random
@@ -59,3 +60,28 @@ def delete_and_create_dir(path):
   if os.path.exists(path):
       shutil.rmtree(path)
   os.makedirs(path)
+
+
+def save_data(filename_with_extension, img_extension, dir_output, img,
+              bounding_boxes, category):
+    filename = os.path.splitext(filename_with_extension)[0]
+    img_path = os.path.join(dir_output, 'images', category,
+                            f'{filename}.{img_extension}')
+    plt.imsave(img_path, img, cmap='gray')
+
+    bbox_path = os.path.join(dir_output, 'labels', category,
+                             filename + '.txt')
+
+    bbox_text = ''
+    for bbox in bounding_boxes:
+        obj_cat = 0
+        center_y = bbox['pos_y'] + bbox['size_y'] / 2
+        center_x = bbox['pos_x'] + bbox['size_x'] / 2
+        center_y_rel = center_y / img.shape[0]
+        center_x_rel = center_x / img.shape[1]
+        size_y_rel = bbox['size_y'] / img.shape[0]
+        size_x_rel = bbox['size_x'] / img.shape[1]
+        bbox_text += (f'{obj_cat} {center_x_rel} {center_y_rel} {size_x_rel} '
+                      f'{size_y_rel}\n')
+    with open(bbox_path, 'w') as file:
+        file.write(bbox_text)
