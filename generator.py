e39d9362cddc29a77d95a6bb65ebd07c913ecd53
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-04-28 19:11:00 -0400

    Add missing newline

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+
 def sample_images_with_qty_prob(imgs, qty_prob):
   qty = np.random.choice(list(qty_prob.keys()), p=list(qty_prob.values()))
   sampled_imgs = random.sample(imgs, qty)
