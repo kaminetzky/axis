6109e62825d5621a978e7852b21397090761d1e3
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-04-28 19:03:26 -0400

    Add get_category function to generator.py

--- generator.py
+++ generator.py
@@ -5,3 +5,15 @@ def sample_images_with_qty_prob(imgs, qty_prob):
   qty = np.random.choice(list(qty_prob.keys()), p=list(qty_prob.values()))
   sampled_imgs = random.sample(imgs, qty)
   return sampled_imgs
+
+
+def get_category(index, total, train_val_ratio):
+  if index < total * train_val_ratio['train']:
+      category = 'train'
+  elif (index < total
+        * (train_val_ratio['train']
+            + train_val_ratio['valid'])):
+      category = 'valid'
+  else:
+      category = 'test'
+  return category
