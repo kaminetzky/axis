ca5c3cc516624fb694a65abef6ed9db8e4b72b7b
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-05-16 16:19:18 -0400

    Add random seed choice (and make non-relevant changes to constants)

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+import numpy as np
+import random
+
 import generator
 
 
-DIR_BGND = 'data/input/data-wrench-real/images/train'
+DIR_BGND = '/Users/ak/Documents/University/Masters/Research/Data/datasets/SIXray/SIXrayN10'
 EXTENSION_BGND = 'jpg'
 
-DIR_FGND_LOW = 'data/input/tests/dual/wrench/wrench-50'
-DIR_FGND_HIGH = 'data/input/tests/dual/wrench/wrench-150'
+DIR_FGND_LOW = 'data/input/wrench-experiments2-flat/dual/wrench-100-test'
+DIR_FGND_HIGH = 'data/input/wrench-experiments2-flat/dual/wrench-150-test'
 EXTENSION_FGND = 'tiff'
 
-DIR_OUT = 'data/output/data-wrench-tests'
-EXTENSION_OUT = 'png'
+DIR_OUT = 'data/output/wrench-sim-only-test'
+EXTENSION_OUT = 'jpg'
 
-LOW_ENERGY = 50
+LOW_ENERGY = 100
 HIGH_ENERGY = 150
 
 SCALE_MIN = 0.3
-SCALE_MAX = 0.6 
+SCALE_MAX = 0.6
 
 FGND_QTY_PROB = {0: 0.1, 1: 0.5, 2: 0.3, 3: 0.1}
 
 TRAIN_VAL_RATIO = {'train': 0.9, 'valid': 0.1}
 
+RANDOM_SEED = 0
+
 
 if __name__ == '__main__':
+  random.seed(RANDOM_SEED)
+  np.random.seed(RANDOM_SEED)
+
   generator.generate_simulated_images(
     DIR_BGND, EXTENSION_BGND, DIR_FGND_LOW, DIR_FGND_HIGH, EXTENSION_FGND,
     DIR_OUT, EXTENSION_OUT, LOW_ENERGY, HIGH_ENERGY, SCALE_MIN, SCALE_MAX,
